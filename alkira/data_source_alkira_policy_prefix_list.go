5a0775138c628e7ced67c3ae97064c8f444a6642
Spike Wang <spike@alkira.com>
2021-03-13 10:29:48 -0800

    AK-14184 Implement data source for policy prefix list
    Add data source support for policy prefix list to all user to use already
    created policy prefix list.

---
+++ alkira/data_source_alkira_policy_prefix_list.go
@@ -0,0 +1,44 @@
+package alkira
+
+import (
+	"strconv"
+
+	"github.com/alkiranet/alkira-client-go/alkira"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceAlkiraPolicyPrefixList() *schema.Resource {
+	return &schema.Resource{
+        Description: "Use this data source to get an existing policy prefix list.",
+
+        Read: dataSourceAlkiraPolicyPrefixListRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description: "The name of the policy prefix list.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"prefixes": {
+				Description: "Prefixes in the prefix list.",
+				Type:        schema.TypeString,
+				Computed:    true,
+				Optional:    true,
+			},
+		},
+	}
+}
+
+func dataSourceAlkiraPolicyPrefixListRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*alkira.AlkiraClient)
+
+	prefixList, err := client.GetPolicyPrefixListByName(d.Get("name").(string))
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(strconv.Itoa(prefixList.Id))
+	d.Set("prefixes", prefixList.Prefixes)
+	return nil
+}
