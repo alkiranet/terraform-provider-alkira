28b5349348388c6303bf4ba7873ecdd4cd536a06
Spike Wang <spike@alkira.com>
2021-06-10 14:27:05 -0700

    AK-16155 Add more documentations and examples for connector-aws-vpc a… (#14)
    + Add more documentation regarding routing options for resource-connector-aws-vpc.
    + Add more examples for data_source_billing_tags.
    + Add release notes for v0.6.1.

--- examples/data-sources/alkira_billing_tag/data-source.tf
+++ examples/data-sources/alkira_billing_tag/data-source.tf
@@ -1,3 +1,26 @@
+#
+# Refer an existing bill tag by name
+#
 data "alkira_billing_tag" "tag1" {
   name = "tag1"
+}
+
+
+#
+# Use it inside any other resources like this connector_aws_vpc.
+#
+resource "alkira_connector_aws_vpc" "test_connector" {
+  name           = "test_vpc"
+  vpc_id         = "test_vpc_id"
+
+  aws_account_id = "test_vpc_aws_account_id"
+  aws_region     = "us-east-1"
+
+  credential_id  = alkira_credential_aws_vpc.account1.id
+  cxp            = "US-WEST"
+  group          = "test"
+  segment        = alkira_segment.test_segment.name
+  size           = "SMALL"
+
+  billing_tags   = [data.alkira_billing_tag.tag1]
 }
