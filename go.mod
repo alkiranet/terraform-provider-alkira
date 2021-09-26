37557d1c673c22c679ca330c5531008cd69f69ea
Spike Wang <spike@alkira.com>
2021-09-26 10:53:41 -0700

    AK-18945 Pick up alkira-client-go v0.7.2 with minor fixes
    Minor fixes for internet application and policy rules.

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/alkiranet/terraform-provider-alkira
 go 1.14
 
 require (
-	github.com/alkiranet/alkira-client-go v0.7.1
+	github.com/alkiranet/alkira-client-go v0.7.2
 	github.com/hashicorp/terraform-plugin-sdk v1.15.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.1.0
 )
--- vendor/github.com/alkiranet/alkira-client-go/alkira/internet_applications.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/internet_applications.go
@@ -3,82 +3,89 @@
 package alkira
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 )
 
-type InternetApplicationRequest struct {
-	BillingTags   []int  `json:"billingTags"`
-	ConnectorId   string `json:"connectorId"`
-	ConnectorType string `json:"connectorType"`
-	FqdnPrefix    string `json:"fqdnPrefix"`
-	Group         string `json:"group"`
-	Name          string `json:"name"`
-	PrivateIp     string `json:"privateIp"`
-	PrivatePort   string `json:"privatePort"`
-	SegmentName   string `json:"segmentName"`
-	Size          string `json:"size"`
-}
-
-type InternetApplicationResponse struct {
-	Id                     int `json:"id"`
-	InboundInternetGroupId int `json:"inboundInternetGroupId"`
+type InternetApplication struct {
+	BillingTags            []int       `json:"billingTags"`
+	ConnectorId            string      `json:"connectorId"`
+	ConnectorType          string      `json:"connectorType"`
+	FqdnPrefix             string      `json:"fqdnPrefix"`
+	Id                     json.Number `json:"id,omitempty"`
+	InboundInternetGroupId json.Number `json:"inboundInternetGroupId,omitempty"`
+	Group                  string      `json:"group"`
+	Name                   string      `json:"name"`
+	PrivateIp              string      `json:"privateIp"`
+	PrivatePort            string      `json:"privatePort"`
+	SegmentName            string      `json:"segmentName"`
+	Size                   string      `json:"size"`
 }
 
 // CreateInternetApplication create an internet application
-func (ac *AlkiraClient) CreateInternetApplication(app *InternetApplicationRequest) (int, int, error) {
+func (ac *AlkiraClient) CreateInternetApplication(app *InternetApplication) (string, string, error) {
 	uri := fmt.Sprintf("%s/v1/tenantnetworks/%s/internet-applications", ac.URI, ac.TenantNetworkId)
-	id := 0
-	groupId := 0
 
 	// Construct the request
 	body, err := json.Marshal(app)
 
-	request, err := http.NewRequest("POST", uri, bytes.NewBuffer(body))
-	request.Header.Set("Content-Type", "application/json")
-	response, err := ac.Client.Do(request)
-
 	if err != nil {
-		return id, groupId, fmt.Errorf("CreateInternetApplication: request failed: %v", err)
+		return "", "", fmt.Errorf("CreateInternetApplication: failed to marshal: %v", err)
 	}
 
-	defer response.Body.Close()
-	data, _ := ioutil.ReadAll(response.Body)
+	data, err := ac.create(uri, body)
 
-	var result InternetApplicationResponse
+	if err != nil {
+		return "", "", err
+	}
+
+	var result InternetApplication
 	json.Unmarshal([]byte(data), &result)
 
-	fmt.Printf("%s", data)
-	if response.StatusCode != 201 {
-		return id, groupId, fmt.Errorf("(%d) %s", response.StatusCode, string(data))
+	if err != nil {
+		return "", "", fmt.Errorf("CreateInternetApplication: failed to unmarshal: %v", err)
 	}
 
-	id = result.Id
-	groupId = result.InboundInternetGroupId
-	return id, groupId, nil
+	return string(result.Id), string(result.InboundInternetGroupId), nil
 }
 
-// DeleteInternetApplication delete given internet application by id
-func (ac *AlkiraClient) DeleteInternetApplication(id int) error {
-	uri := fmt.Sprintf("%s/v1/tenantnetworks/%s/internet-applications/%d", ac.URI, ac.TenantNetworkId, id)
+// DeleteInternetApplication delete given internet application by Id
+func (ac *AlkiraClient) DeleteInternetApplication(id string) error {
+	uri := fmt.Sprintf("%s/v1/tenantnetworks/%s/internet-applications/%s", ac.URI, ac.TenantNetworkId, id)
 
-	request, err := http.NewRequest("DELETE", uri, nil)
-	request.Header.Set("Content-Type", "application/json")
-	response, err := ac.Client.Do(request)
+	return ac.delete(uri)
+}
+
+// UpdateInternetApplication update a given internet application by Id
+func (ac *AlkiraClient) UpdateInternetApplication(id string, app *InternetApplication) error {
+	uri := fmt.Sprintf("%s/v1/tenantnetworks/%s/internet-applications/%s", ac.URI, ac.TenantNetworkId, id)
+
+	// Construct the request
+	body, err := json.Marshal(app)
 
 	if err != nil {
-		return fmt.Errorf("DeleteInternetApplication: request failed: %v", err)
+		return fmt.Errorf("UpdateInternetApplication: failed to marshal: %v", err)
 	}
 
-	defer response.Body.Close()
-	data, _ := ioutil.ReadAll(response.Body)
+	return ac.update(uri, body)
+}
 
-	if response.StatusCode != 200 {
-		return fmt.Errorf("(%d) %s", response.StatusCode, string(data))
+// GetInternetApplication get internet application by Id
+func (ac *AlkiraClient) GetInternetApplication(id string) (*InternetApplication, error) {
+	uri := fmt.Sprintf("%s/v1/tenantnetworks/%s/internet-applications/%s", ac.URI, ac.TenantNetworkId, id)
+
+	data, err := ac.get(uri)
+
+	if err != nil {
+		return nil, err
 	}
 
-	return nil
+	var result InternetApplication
+	err = json.Unmarshal([]byte(data), &result)
+
+	if err != nil {
+		return nil, fmt.Errorf("GetInternetApplication: failed to unmarshal: %v", err)
+	}
+
+	return &result, nil
 }
