f3aa37a35f1bf86702bf44f5c2fe15089f405e15
mac-alkira <macdonald@alkira.com>
2022-02-22 11:37:16 -0700

    fix dependency issues

--- alkira/resource_connector_gcp_vpc_helper.go
+++ alkira/resource_connector_gcp_vpc_helper.go
@@ -1,15 +1,10 @@
 package alkira
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/alkiranet/alkira-client-go/alkira"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-
-	//"github.com/zclconf/go-cty/cty"
-	"github.com/hashicorp/go-cty/cty"
 )
 
 func convertGcpRouting(in *schema.Set) *alkira.ConnectorGcpVpcRouting {
@@ -39,26 +34,4 @@ func convertGcpRouting(in *schema.Set) *alkira.ConnectorGcpVpcRouting {
 	}
 
 	return gcp
-}
-
-func validateCustomPrefix(v interface{}, p cty.Path) diag.Diagnostics {
-	value := v.(string)
-	var diags diag.Diagnostics
-
-	if value == "ADVERTISE_CUSTOM_PREFIX" {
-		return diags
-	}
-
-	if value == "ADVERTISE_DEFAULT_ROUTE" {
-		return diags
-	}
-
-	diag := diag.Diagnostic{
-		Severity: diag.Error,
-		Summary:  "Invalid custom_prefix value. Valid custom_prefix options are ADVERTISE_CUSTOM_PREFIX ADVERTISE_DEFAULT_ROUTE.",
-		Detail:   fmt.Sprintf("Invalid custom_prefix value: %q. Only ADVERTISE_CUSTOM_PREFIX or ADVERTISE_DEFAULT_ROUTE are accepted values.", value),
-	}
-	diags = append(diags, diag)
-
-	return diags
 }
