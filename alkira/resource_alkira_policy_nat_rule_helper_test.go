5f927d17027b04f02239acd978f915b85f715bc4
mac-alkira <macdonald@alkira.com>
2022-03-09 15:31:17 -0700

    fix for properly reading in List inputs to Nat Rule data structs

---
+++ alkira/resource_alkira_policy_nat_rule_helper_test.go
@@ -0,0 +1,101 @@
+package alkira
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/k0kubun/pp"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPolicyNatRuleExpandMatchArrays(t *testing.T) {
+	expectedStrArr := []interface{}{"1", "2", "3"}
+	expectedIntArr := []interface{}{1, 2, 3}
+
+	m := makeMapPolicyNatRuleMatch(expectedStrArr, expectedIntArr)
+	mArr := []interface{}{m}
+
+	r := resourceAlkiraPolicyNatRule()
+	f := schema.HashResource(r)
+	s := schema.NewSet(f, mArr)
+
+	actual := expandPolicyNatRuleMatch(s)
+
+	require.Equal(t, convertTypeListToStringList(expectedStrArr), actual.SourcePrefixes)
+	require.Equal(t, convertTypeListToIntList(expectedIntArr), actual.SourcePrefixListIds)
+	require.Equal(t, convertTypeListToStringList(expectedStrArr), actual.SourcePortList)
+
+	require.Equal(t, convertTypeListToStringList(expectedStrArr), actual.DestPrefixes)
+	require.Equal(t, convertTypeListToIntList(expectedIntArr), actual.DestPrefixListIds)
+	require.Equal(t, convertTypeListToStringList(expectedStrArr), actual.DestPortList)
+}
+
+func TestPolicyNatRuleExpandNatRuleActionArrays(t *testing.T) {
+	expectedStrArr := []interface{}{"1", "2", "3"}
+	expectedIntArr := []interface{}{1, 2, 3}
+
+	m := makeMapPolicyNatRuleAction(expectedStrArr, expectedIntArr)
+	mArr := []interface{}{m}
+
+	r := resourceAlkiraPolicyNatRule()
+	f := schema.HashResource(r)
+	s := schema.NewSet(f, mArr)
+
+	actual := expandPolicyNatRuleAction(s)
+	pp.Println(actual)
+
+	//Src array validations
+	require.Equal(t,
+		convertTypeListToStringList(expectedStrArr),
+		actual.SourceAddressTranslation.TranslatedPrefixes,
+	)
+	require.Equal(t,
+		convertTypeListToIntList(expectedIntArr),
+		actual.SourceAddressTranslation.TranslatedPrefixListIds,
+	)
+
+	//Dst array validations
+	require.Equal(t,
+		convertTypeListToStringList(expectedStrArr),
+		actual.DestinationAddressTranslation.TranslatedPrefixes,
+	)
+	require.Equal(t,
+		convertTypeListToStringList(expectedStrArr),
+		actual.DestinationAddressTranslation.TranslatedPortList,
+	)
+	require.Equal(t,
+		convertTypeListToIntList(expectedIntArr),
+		actual.DestinationAddressTranslation.TranslatedPrefixListIds,
+	)
+
+}
+
+//
+// TEST HELPERS
+//
+
+func makeMapPolicyNatRuleAction(strArr, intArr []interface{}) map[string]interface{} {
+	m := make(map[string]interface{})
+
+	m["src_addr_translation_prefixes"] = strArr
+	m["src_addr_translation_prefix_list_ids"] = intArr
+
+	m["dst_addr_translation_prefixes"] = strArr
+	m["dst_addr_translation_prefix_list_ids"] = intArr
+	m["dst_addr_translation_ports"] = strArr
+
+	return m
+}
+
+func makeMapPolicyNatRuleMatch(strArr, intArr []interface{}) map[string]interface{} {
+	m := make(map[string]interface{})
+	m["src_prefixes"] = strArr
+	m["src_prefix_list_ids"] = intArr
+	m["src_ports"] = strArr
+	m["dst_prefixes"] = strArr
+	m["dst_prefix_list_ids"] = intArr
+	m["dst_ports"] = strArr
+	m["protocol"] = "ANY"
+
+	return m
+}
