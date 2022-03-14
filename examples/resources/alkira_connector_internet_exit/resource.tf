ed4bcd57d478e944ddb3411bd42b843459a29346
Spike Wang <spike@alkira.com>
2022-03-14 09:32:56 -0700

    AK-21257 Update alkira_internet_application with latest features (#91)
    Update alkira_internet_application with latest features.
    
    + add target block.
    + add inbound arguments.
    + add public_ips.
    - private_ip and private_ports are deprecated.

--- examples/resources/alkira_connector_internet_exit/resource.tf
+++ examples/resources/alkira_connector_internet_exit/resource.tf
@@ -2,7 +2,7 @@ resource "alkira_connector_internet_exit" "test1" {
   name           = "test1"
   cxp            = "US-WEST"
   group          = "group1"
-  segment        = "seg1"
+  segment_id     = alkira_segment.test.id
   size           = "SMALL"
 }
 
