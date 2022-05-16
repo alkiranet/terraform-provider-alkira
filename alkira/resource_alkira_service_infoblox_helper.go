aeb5f884b3a57c6a9f33228fb0c57ffbb5e4f2f1
mac-alkira <macdonald@alkira.com>
2022-05-16 17:55:50 -0600

    full terraform functionality with unit tests

---
+++ alkira/resource_alkira_service_infoblox_helper.go
@@ -0,0 +1,147 @@
+package alkira
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/alkiranet/alkira-client-go/alkira"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func expandInfobloxInstances(in *schema.Set) []alkira.InfobloxInstance {
+	if in == nil || in.Len() == 0 {
+		log.Printf("[DEBUG] invalid Infoblox instance input")
+		return nil
+	}
+
+	instances := make([]alkira.InfobloxInstance, in.Len())
+	for i, instance := range in.List() {
+		r := alkira.InfobloxInstance{}
+		instanceCfg := instance.(map[string]interface{})
+		if v, ok := instanceCfg["any_cast_enabled"].(bool); ok {
+			r.AnyCastEnabled = v
+		}
+		if v, ok := instanceCfg["credential_id"].(string); ok {
+			r.CredentialId = v
+		}
+		if v, ok := instanceCfg["host_name"].(string); ok {
+			r.HostName = v
+		}
+		if v, ok := instanceCfg["model"].(string); ok {
+			r.Model = v
+		}
+		if v, ok := instanceCfg["type"].(string); ok {
+			r.Type = v
+		}
+		if v, ok := instanceCfg["version"].(string); ok {
+			r.Version = v
+		}
+		instances[i] = r
+	}
+
+	return instances
+}
+
+func deflateInfobloxInstances(c []alkira.InfobloxInstance) []map[string]interface{} {
+	var instances []map[string]interface{}
+
+	for _, instance := range c {
+		i := map[string]interface{}{
+			"any_cast_enabled": instance.AnyCastEnabled,
+			"credential_id":    instance.CredentialId,
+			"host_name":        instance.HostName,
+			"model":            instance.Model,
+			"type":             instance.Type,
+			"version":          instance.Version,
+		}
+		instances = append(instances, i)
+	}
+
+	return instances
+}
+
+func expandInfobloxGridMaster(in *schema.Set) (*alkira.InfobloxGridMaster, error) {
+	if in == nil || in.Len() > 1 || in.Len() < 1 {
+		return nil, fmt.Errorf("[DEBUG] Exactly one object allowed in grid master options.")
+	}
+
+	im := &alkira.InfobloxGridMaster{}
+
+	for _, option := range in.List() {
+		cfg := option.(map[string]interface{})
+		if v, ok := cfg["external"].(bool); ok {
+			im.External = v
+		}
+		if v, ok := cfg["grid_master_credential_id"].(string); ok {
+			im.GridMasterCredentialId = v
+		}
+		if v, ok := cfg["ip"].(string); ok {
+			im.Ip = v
+		}
+		if v, ok := cfg["name"].(string); ok {
+			im.Name = v
+		}
+		if v, ok := cfg["shared_secret_credential_id"].(string); ok {
+			im.SharedSecretCredentialId = v
+		}
+	}
+	return im, nil
+}
+
+func deflateInfobloxGridMaster(im alkira.InfobloxGridMaster) []map[string]interface{} {
+	m := make(map[string]interface{})
+	m["external"] = im.External
+	m["grid_master_credential_id"] = im.GridMasterCredentialId
+	m["ip"] = im.Ip
+	m["name"] = im.Name
+	m["shared_secret_credential_id"] = im.SharedSecretCredentialId
+
+	return []map[string]interface{}{m}
+}
+
+func expandInfobloxAnycast(in *schema.Set) (*alkira.InfobloxAnycast, error) {
+	if in == nil || in.Len() > 1 || in.Len() < 1 {
+		return nil, fmt.Errorf("[DEBUG] Exactly one object allowed in anycast options.")
+	}
+
+	ia := &alkira.InfobloxAnycast{}
+
+	for _, option := range in.List() {
+		cfg := option.(map[string]interface{})
+		//convertTypeListToIntList(d.Get("billing_tag_ids").([]interface{}))
+		if v, ok := cfg["enabled"].(bool); ok {
+			ia.Enabled = v
+		}
+		if v, ok := cfg["ips"].([]interface{}); ok {
+			ia.Ips = convertTypeListToStringList(v)
+		}
+		if v, ok := cfg["backup_cxps"].([]interface{}); ok {
+			ia.BackupCxps = convertTypeListToStringList(v)
+		}
+	}
+	return ia, nil
+
+}
+
+func deflateInfobloxAnycast(ia alkira.InfobloxAnycast) []map[string]interface{} {
+	m := make(map[string]interface{})
+	m["enabled"] = ia.Enabled
+	m["ips"] = ia.Ips
+	m["backup_cxps"] = ia.BackupCxps
+
+	return []map[string]interface{}{m}
+}
+
+func setAllInfobloxResourceFields(d *schema.ResourceData, in *alkira.Infoblox) {
+	d.Set("anycast", deflateInfobloxAnycast(in.AnyCast))
+	d.Set("billing_tag_ids", in.BillingTags)
+	d.Set("cxp", in.Cxp)
+	d.Set("description", in.Description)
+	d.Set("global_cidr_list_id", in.GlobalCidrListId)
+	d.Set("grid_master", deflateInfobloxGridMaster(in.GridMaster))
+	d.Set("instances", deflateInfobloxInstances(in.Instances))
+	d.Set("license_type", in.LicenseType)
+	d.Set("segment_names", in.Segments)
+	d.Set("service_group_name", in.ServiceGroupName)
+	d.Set("size", in.Size)
+}
---
+++ alkira/resource_alkira_service_infoblox_test.go
@@ -0,0 +1,116 @@
+package alkira
+
+import (
+	"encoding/json"
+	"sort"
+	"strconv"
+	"testing"
+
+	"github.com/alkiranet/alkira-client-go/alkira"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExpandDeflateInfoblox(t *testing.T) {
+	expectedInstances := makeInfobloxInstances(4, true)
+	expectedGridMaster := makeInfobloxGridMaster("gm", false)
+	expectedAnycast := makeInfobloxAnycast(3, 5, "backupVal", "ipsVal", false)
+
+	expectedInfoblox := &alkira.Infoblox{
+		AnyCast:    expectedAnycast,
+		Instances:  expectedInstances,
+		GridMaster: expectedGridMaster,
+	}
+
+	d := resourceAlkiraInfoblox().TestResourceData()
+	setAllInfobloxResourceFields(d, expectedInfoblox)
+
+	actualInfoblox, err := generateInfobloxRequest(d, nil)
+	require.NoError(t, err)
+
+	require.Equal(t, expectedGridMaster, actualInfoblox.GridMaster)
+	require.Equal(t, expectedAnycast, actualInfoblox.AnyCast)
+	requireInfobloxInstancesEqual(t, expectedInstances, actualInfoblox.Instances)
+}
+
+func makeInfobloxInstances(num int, anycast bool) []alkira.InfobloxInstance {
+	var ins []alkira.InfobloxInstance
+	for i := 0; i < num; i++ {
+		in := makeInfobloxInstance(strconv.Itoa(i), i, anycast)
+		ins = append(ins, in)
+	}
+
+	return ins
+}
+
+func makeInfobloxInstance(prefix string, id int, anycast bool) alkira.InfobloxInstance {
+	return alkira.InfobloxInstance{
+		AnyCastEnabled:     anycast,
+		ConfiguredMasterIp: prefix + "ConfiguredMasterIp",
+		CredentialId:       prefix + "CredentialId",
+		HostName:           prefix + "HostName",
+		Id:                 json.Number(strconv.Itoa(id)),
+		InternalName:       prefix + "InternalName",
+		LanPrefix:          prefix + "LanPrefix",
+		ManagementPrefix:   prefix + "ManagementPrefix",
+		Model:              prefix + "Model",
+		Name:               prefix + "Name",
+		ProductId:          prefix + "ProductId",
+		PublicIp:           prefix + "PublicIp",
+		Type:               prefix + "Type",
+		Version:            prefix + "Version",
+	}
+}
+
+func makeInfobloxGridMaster(prefix string, external bool) alkira.InfobloxGridMaster {
+	return alkira.InfobloxGridMaster{
+		External:                 external,
+		GridMasterCredentialId:   prefix + "GridMasterCredentialId",
+		Ip:                       prefix + "Ip",
+		Name:                     prefix + "Name",
+		SharedSecretCredentialId: prefix + "SharedSecretCredentialId",
+	}
+}
+
+func makeInfobloxAnycast(lenBackups, lenIps int, backupValue, ipsValue string, external bool) alkira.InfobloxAnycast {
+	backups := make([]string, lenBackups)
+	for i, _ := range backups {
+		backups[i] = strconv.Itoa(i) + backupValue
+	}
+
+	ips := make([]string, lenIps)
+	for i, _ := range ips {
+		ips[i] = strconv.Itoa(i) + ipsValue
+	}
+
+	return alkira.InfobloxAnycast{
+		BackupCxps: backups,
+		Enabled:    external,
+		Ips:        ips,
+	}
+}
+
+func sortInfobloxInstancesByCredentialId(ins []alkira.InfobloxInstance) {
+	sort.Slice(ins, func(i, j int) bool { return ins[i].CredentialId < ins[j].CredentialId })
+}
+
+//This is required because the infoblox client has quite a few fields that are not currently in use
+//in the backend. Here we are making comparisons only to the fields that the terraform user might
+//input.
+func requireInfobloxInstancesEqual(t *testing.T, expected, actual []alkira.InfobloxInstance) {
+	require.Equal(t, len(expected), len(actual))
+
+	//It doesn't really matter how we sort as long as they are in the same order. I chose
+	//credentialId because it has the most potential for uniqueness i.e. the model field could
+	//conceivably all be the same.
+	sortInfobloxInstancesByCredentialId(expected)
+	sortInfobloxInstancesByCredentialId(actual)
+
+	for i, v := range actual {
+		require.Equal(t, expected[i].AnyCastEnabled, v.AnyCastEnabled)
+		require.Equal(t, expected[i].CredentialId, v.CredentialId)
+		require.Equal(t, expected[i].HostName, v.HostName)
+		require.Equal(t, expected[i].Model, v.Model)
+		require.Equal(t, expected[i].Type, v.Type)
+		require.Equal(t, expected[i].Version, v.Version)
+	}
+}
