a0e21bfc91f9c8c2438f905e324026c4e624da8e
Spike Wang <spike@alkira.com>
2022-08-16 15:03:02 -0700

    AK-27284 Fix the example of alkira_segment_resource
    Fix the example in documentation of alkira_segment_resource.

--- examples/resources/alkira_segment_resource/resource.tf
+++ examples/resources/alkira_segment_resource/resource.tf
@@ -3,7 +3,7 @@ resource "alkira_segment_resource" "test" {
   segment_id = alkira_segment.tftest.id
 
   group_prefix {
-    connector_group_id = -1
-    prefix_list_id     = -1
+    group_id       = -1
+    prefix_list_id = -1
   }
 }
