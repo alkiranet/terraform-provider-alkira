d5cb8e06319b11f9ea49aef999e162d134d6fe15
Alex Lin <alex.lin@alkira.net>
2022-08-23 20:07:41 -0400

    AK-27516 Creating OCI VCN data source.

---
+++ alkira/data_source_alkira_oci_vcn_connector.go
@@ -0,0 +1,35 @@
+package alkira
+
+import (
+	"github.com/alkiranet/alkira-client-go/alkira"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceAlkiraOciVcnConnector() *schema.Resource {
+	return &schema.Resource{
+		Description: "Use this data source to get an existing connector group.",
+
+		Read: dataSourceAlkiraOciVcnConnectorRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description: "The name of the group.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+		},
+	}
+}
+
+func dataSourceAlkiraOciVcnConnectorRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*alkira.AlkiraClient)
+
+	group, err := client.GetConnectorOciVcn(d.Get("name").(string))
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(string(group.Id))
+	return nil
+}
