1852945591832b6d2e289b38bdcbcd277dd496ad
Mac Farnsowrth <97978598+mac-alkira@users.noreply.github.com>
2022-10-06 07:57:28 -0600

    AK-28550 Add management server password field for checkpoint credential (#192)

--- examples/resources/alkira_credential_checkpoint/resource.tf
+++ examples/resources/alkira_credential_checkpoint/resource.tf
@@ -1,5 +1,6 @@
 resource "alkira_credential_checkpoint" "tf_test_checkpoint" {
-  name     = "tf-test-checkpoint"
-  password = "Ak12345678"
-  sic_keys = ["88888888888", "999999999"]
+  name                       = "tf-test-checkpoint"
+  password                   = "Ak12345678"
+  management_server_password = "MGMTPSWD111"
+  sic_keys                   = ["AAAAA88888888888", "BBBBB999999999"]
 }
