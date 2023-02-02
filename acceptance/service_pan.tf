bae5e5e7d93280cdd61b01cc3ead674706fbcd61
Spike Wang <spike@alkira.com>
2023-02-02 12:50:31 -0800

    Update the service-pan version in acceptance test

--- acceptance/service_pan.tf
+++ acceptance/service_pan.tf
@@ -15,7 +15,7 @@ resource "alkira_service_pan" "test" {
   management_segment_id = alkira_segment.test1.id
   size                  = "SMALL"
   type                  = "VM-300"
-  version               = "9.1.3"
+  version               = "10.2.2-h2"
 
   registration_pin_id     = "1234567890ABCDEF"
   registration_pin_value  = "1234567890ABCDEF"
