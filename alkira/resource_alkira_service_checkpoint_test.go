9f99e42c0e6c10f20d384434b7c58f94407ac717
Cameron Richards <Cameron.Richards@alkira.net>
2023-02-07 11:40:47 -0500

    AK-29987 Fix formatting issues

--- alkira/resource_alkira_service_checkpoint_test.go
+++ alkira/resource_alkira_service_checkpoint_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/alkiranet/alkira-client-go/alkira"
 )
 
+//
 // TEST HELPER
+//
 func serveCheckpoint(t *testing.T, c *alkira.Checkpoint) *alkira.AlkiraClient {
 	server := httptest.NewServer(http.HandlerFunc(
 		func(w http.ResponseWriter, req *http.Request) {
