e4be633129fee0c5f9616714b729ae4832817346
Spike Wang <spike@alkira.com>
2023-02-28 13:13:35 -0800

    Adjust the name in the config

--- acceptance/service_fortinet.tf
+++ acceptance/service_fortinet.tf
@@ -1,19 +1,19 @@
 resource "alkira_service_fortinet" "test1" {
   username                     = "admin"
   password                     = "Ak12345678"
-  cxp                          = "US-WEST"
+  cxp                          = "US-WEST-1"
   license_type                 = "BRING_YOUR_OWN"
   management_server_segment_id = alkira_segment.test1.id
   max_instance_count           = 1
   min_instance_count           = 1
-  name                         = "acceptance-fortinet-test1"
+  name                         = "a-test1"
   segment_ids                  = [alkira_segment.test1.id, alkira_segment.test2.id]
   size                         = "SMALL"
   tunnel_protocol              = "IPSEC"
   version                      = "7.0.3"
 
   instances {
-    name                  = "acceptance-fortinet-instance-1"
+    name                  = "a-ins1"
     serial_number         = "test-instance-1"
     license_key_file_path = "fortinet-test.license"
   }
