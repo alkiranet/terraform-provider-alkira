c25f2c27e700aca78fe9da9905ae40f87367e177
Spike Wang <spike@alkira.com>
2023-09-18 14:01:35 -0700

    AK-38078 Improve read function of connector_aws_vpc (#292)
    + Improve the read functionality of connector_aws_vpc to read tgw_attachement
    better.
    + Switch to TypeSet for unordered array to avoid diff.
    + Update documentation.

--- alkira/resource_alkira_connector_aws_vpc_helper.go
+++ alkira/resource_alkira_connector_aws_vpc_helper.go
@@ -8,6 +8,57 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+// setTgwAttachement set tgw_attachement blocks
+func setTgwAttachment(d *schema.ResourceData, tgwAttachments []alkira.TgwAttachment) {
+
+	var attachments []map[string]interface{}
+
+	for _, each := range d.Get("tgw_attachment").([]interface{}) {
+		a := each.(map[string]interface{})
+
+		for _, cfg := range tgwAttachments {
+			if a["subnet_id"].(string) == cfg.SubnetId {
+				attachments = append(attachments, a)
+				break
+			}
+		}
+	}
+
+	//
+	// Go through all tgw_attachments from the API response one more
+	// time to find any attachement that has not been tracked from
+	// Terraform config.
+	//
+	for _, cfg := range tgwAttachments {
+		new := true
+
+		// Check if the gateway already exists in the Terraform config
+		for _, tgw := range d.Get("tgw_attachement").([]interface{}) {
+			a := tgw.(map[string]interface{})
+
+			if a["subnet_id"].(string) == cfg.SubnetId {
+				new = false
+				break
+			}
+		}
+
+		// If the attachment is new, add it to the tail of the list,
+		// this will generate a diff
+		if new {
+			attachment := map[string]interface{}{
+				"subnet_id": cfg.SubnetId,
+				"az":        cfg.AvailabilityZone,
+			}
+
+			attachments = append(attachments, attachment)
+			break
+		}
+	}
+
+	d.Set("tgw_attachment", attachments)
+
+}
+
 // expandAwsVpcRouteTables expand AWS-VPC route tables
 func expandAwsVpcRouteTables(in *schema.Set) []alkira.RouteTables {
 	if in == nil || in.Len() == 0 {
@@ -26,15 +77,15 @@ func expandAwsVpcRouteTables(in *schema.Set) []alkira.RouteTables {
 			r.Mode = v
 		}
 
-		r.PrefixListIds = convertTypeListToIntList(t["prefix_list_ids"].([]interface{}))
+		r.PrefixListIds = convertTypeSetToIntList(t["prefix_list_ids"].(*schema.Set))
 		tables[i] = r
 	}
 
 	return tables
 }
 
-// generateUserInputPrefixes generate UserInputPrefixes used in AWS-VPC connector
-func generateUserInputPrefixes(cidr []interface{}, subnets *schema.Set) ([]alkira.InputPrefixes, error) {
+// expandUserInputPrefixes generate UserInputPrefixes used in AWS-VPC connector
+func expandUserInputPrefixes(cidr []interface{}, subnets *schema.Set) ([]alkira.InputPrefixes, error) {
 
 	if len(cidr) == 0 && subnets == nil {
 		return nil, fmt.Errorf("ERROR: either \"vpc_subnet\" or \"vpc_cidr\" must be specified.")
@@ -79,16 +130,16 @@ func generateUserInputPrefixes(cidr []interface{}, subnets *schema.Set) ([]alkir
 }
 
 // expandAwsVpcTgwAttachments expand tgw_attachment of connector_aws_vpc
-func expandAwsVpcTgwAttachments(in *schema.Set) []alkira.TgwAttachment {
-	if in == nil || in.Len() == 0 {
-		log.Printf("[DEBUG] Empty VPC route table input")
+func expandAwsVpcTgwAttachments(in []interface{}) []alkira.TgwAttachment {
+	if in == nil || len(in) == 0 {
 		return []alkira.TgwAttachment{}
 	}
 
-	attachments := make([]alkira.TgwAttachment, in.Len())
-	for i, attachment := range in.List() {
+	attachments := make([]alkira.TgwAttachment, len(in))
+	for i, attachment := range in {
 		r := alkira.TgwAttachment{}
 		t := attachment.(map[string]interface{})
+
 		if v, ok := t["subnet_id"].(string); ok {
 			r.SubnetId = v
 		}
@@ -100,4 +151,56 @@ func expandAwsVpcTgwAttachments(in *schema.Set) []alkira.TgwAttachment {
 	}
 
 	return attachments
+}
+
+// generateConnectorAwsVpcRequest generate request for connector_aws_vpc
+func generateConnectorAwsVpcRequest(d *schema.ResourceData, m interface{}) (*alkira.ConnectorAwsVpc, error) {
+
+	// Segment
+	segmentName, err := getSegmentNameById(d.Get("segment_id").(string), m)
+
+	if err != nil {
+		return nil, err
+	}
+
+	inputPrefixes, err := expandUserInputPrefixes(d.Get("vpc_cidr").([]interface{}), d.Get("vpc_subnet").(*schema.Set))
+
+	if err != nil {
+		return nil, err
+	}
+
+	exportOptions := alkira.ExportOptions{
+		Mode:     "USER_INPUT_PREFIXES",
+		Prefixes: inputPrefixes,
+	}
+
+	routeTables := expandAwsVpcRouteTables(d.Get("vpc_route_table").(*schema.Set))
+	tgwAttachments := expandAwsVpcTgwAttachments(d.Get("tgw_attachment").([]interface{}))
+
+	vpcRouting := alkira.ConnectorAwsVpcRouting{
+		Export: exportOptions,
+		Import: alkira.ImportOptions{routeTables},
+	}
+
+	request := &alkira.ConnectorAwsVpc{
+		BillingTags:                        convertTypeSetToIntList(d.Get("billing_tag_ids").(*schema.Set)),
+		CXP:                                d.Get("cxp").(string),
+		CredentialId:                       d.Get("credential_id").(string),
+		CustomerName:                       m.(*alkira.AlkiraClient).Username,
+		CustomerRegion:                     d.Get("aws_region").(string),
+		DirectInterVPCCommunicationEnabled: d.Get("direct_inter_vpc_communication").(bool),
+		Enabled:                            d.Get("enabled").(bool),
+		Group:                              d.Get("group").(string),
+		Name:                               d.Get("name").(string),
+		Segments:                           []string{segmentName},
+		SecondaryCXPs:                      convertTypeSetToStringList(d.Get("failover_cxps").(*schema.Set)),
+		Size:                               d.Get("size").(string),
+		TgwConnectEnabled:                  d.Get("tgw_connect_enabled").(bool),
+		TgwAttachments:                     tgwAttachments,
+		VpcId:                              d.Get("vpc_id").(string),
+		VpcOwnerId:                         d.Get("aws_account_id").(string),
+		VpcRouting:                         vpcRouting,
+	}
+
+	return request, nil
 }
