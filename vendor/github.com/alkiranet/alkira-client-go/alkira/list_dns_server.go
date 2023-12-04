b5353c5dda220c8ebbcd3ce91543a649c461f328
Spike Wang <spike@alkira.com>
2023-12-03 23:03:20 -0800

    AK-38607 Pick up alkira-client-go v1.45.1
    Pick up alkira-client-go v1.45.1 with new APIs for:
    
    + policy-fqdn-list
    + dns-server-list
    + connector-ipsec-tunnel-profile

---
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/list_dns_server.go
@@ -0,0 +1,23 @@
+// Copyright (C) 2023 Alkira Inc. All Rights Reserved.
+
+package alkira
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type DnsServerList struct {
+	Id           json.Number `json:"id,omitempty"`
+	Name         string      `json:"name"`
+	Description  string      `json:"description"`
+	DnsServerIps []string    `json:"dnsServerIps"`
+	Segment      string      `json:"segment"`
+}
+
+// NewDnsServerList new DNS server list
+func NewDnsServerList(ac *AlkiraClient) *AlkiraAPI[DnsServerList] {
+	uri := fmt.Sprintf("%s/tenantnetworks/%s/dns-server-lists", ac.URI, ac.TenantNetworkId)
+	api := &AlkiraAPI[DnsServerList]{ac, uri, true}
+	return api
+}
---
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/list_policy_fqdn.go
@@ -0,0 +1,23 @@
+// Copyright (C) 2023 Alkira Inc. All Rights Reserved.
+
+package alkira
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type PolicyFqdnList struct {
+	Id              json.Number `json:"id,omitempty"`
+	Name            string      `json:"name"`
+	Description     string      `json:"description"`
+	Fqdns           []string    `json:"fqdns"`
+	DnsServerListId int         `json:"dnsServerListId"`
+}
+
+// NewPolicyFqdnList new global cidr list
+func NewPolicyFqdnList(ac *AlkiraClient) *AlkiraAPI[PolicyFqdnList] {
+	uri := fmt.Sprintf("%s/tenantnetworks/%s/policy-fqdn-lists", ac.URI, ac.TenantNetworkId)
+	api := &AlkiraAPI[PolicyFqdnList]{ac, uri, true}
+	return api
+}
