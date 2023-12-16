c5547734240e117e6e85a0c7f9248af8fb43aefc
Spike Wang <spike@alkira.com>
2023-12-16 15:59:59 -0800

    AK-38608 Add new optional field to NAT policy rule
    Add new optional field to specify list_policy_fqdn in policy_nat_rule.

--- vendor/github.com/alkiranet/alkira-client-go/alkira/segment.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/segment.go
@@ -26,17 +26,18 @@ type ServiceTrafficDistribution struct {
 }
 
 type Segment struct {
-	Asn                                        int                        `json:"asn"`
-	Description                                string                     `json:"description,omitempty"`
-	EnableIpv6ToIpv4Translation                bool                       `json:"enableIpv6ToIpv4Translation"`
-	EnterpriseDNSServerIP                      string                     `json:"enterpriseDNSServerIP,omitempty"`
-	Id                                         json.Number                `json:"id,omitempty"` // only for response
-	IpBlock                                    string                     `json:"ipBlock,omitempty"`
-	IpBlocks                                   SegmentIpBlocks            `json:"ipBlocks,omitempty"`
-	Name                                       string                     `json:"name"`
-	ReservePublicIPsForUserAndSiteConnectivity bool                       `json:"reservePublicIPsForUserAndSiteConnectivity,omitempty"`
-	SrcIpv4PoolList                            []SegmentSrcIpv4PoolList   `json:"srcIpv4PoolList,omitempty"`
-	ServiceTrafficDistribution                 ServiceTrafficDistribution `json:"serviceTrafficDistribution,omitempty"`
+	Asn                                               int                        `json:"asn"`
+	Description                                       string                     `json:"description,omitempty"`
+	EnableIpv6ToIpv4Translation                       bool                       `json:"enableIpv6ToIpv4Translation"`
+	EnterpriseDNSServerIP                             string                     `json:"enterpriseDNSServerIP,omitempty"`
+	Id                                                json.Number                `json:"id,omitempty"` // only for response
+	IpBlock                                           string                     `json:"ipBlock,omitempty"`
+	IpBlocks                                          SegmentIpBlocks            `json:"ipBlocks,omitempty"`
+	Name                                              string                     `json:"name"`
+	ReservePublicIPsForUserAndSiteConnectivity        bool                       `json:"reservePublicIPsForUserAndSiteConnectivity,omitempty"`
+	ReservePublicIPsForUserAndSiteConnectivityForCXPs []string                   `json:"reservePublicIPsForUserAndSiteConnectivityForCXPs,omitempty"`
+	SrcIpv4PoolList                                   []SegmentSrcIpv4PoolList   `json:"srcIpv4PoolList,omitempty"`
+	ServiceTrafficDistribution                        ServiceTrafficDistribution `json:"serviceTrafficDistribution,omitempty"`
 }
 
 func NewSegment(ac *AlkiraClient) *AlkiraAPI[Segment] {
