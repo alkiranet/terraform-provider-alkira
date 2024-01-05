72f0e71900623632f57d6dea0221ecdd16369bfe
Spike Wang <spike@alkira.com>
2024-01-04 22:44:26 -0800

    AK-41082 Update the ID of ip_reservation resource
    Update the ID to string for ip_reservation resource.

--- vendor/github.com/alkiranet/alkira-client-go/alkira/ip_reservations.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/ip_reservations.go
@@ -8,17 +8,17 @@ import (
 )
 
 type IPReservation struct {
-	Id                json.Number `json:"id,omitempty"`
-	Name              string      `json:"name"`
-	Type              string      `json:"type"`
-	Prefix            string      `json:"prefix"`
-	PrefixLen         int         `json:"prefixLen"`
-	PrefixType        string      `json:"prefixType"`
-	FirstIpAssignedTo string      `json:"firstIpAssignedTo"`
-	NodeId            string      `json:"nodeId"`
-	Cxp               string      `json:"cxp"`
-	ScaleGroupId      string      `json:"scaleGroupId"`
-	Segment           string      `json:"segment"`
+	Id                string `json:"id,omitempty"`
+	Name              string `json:"name"`
+	Type              string `json:"type"`
+	Prefix            string `json:"prefix"`
+	PrefixLen         int    `json:"prefixLen"`
+	PrefixType        string `json:"prefixType"`
+	FirstIpAssignedTo string `json:"firstIpAssignedTo"`
+	NodeId            string `json:"nodeId"`
+	Cxp               string `json:"cxp"`
+	ScaleGroupId      string `json:"scaleGroupId"`
+	Segment           string `json:"segment"`
 }
 
 func NewIPReservation(ac *AlkiraClient) *AlkiraAPI[IPReservation] {
