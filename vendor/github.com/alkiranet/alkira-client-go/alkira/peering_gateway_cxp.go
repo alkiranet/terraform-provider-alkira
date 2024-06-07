02295653cf1b23196bb3b1b83204a9d80d282e78
Asim Ali Khan <asim.ali_khan@alkira.net>
2024-06-07 14:44:08 +0530

    remove segmentid

--- vendor/github.com/alkiranet/alkira-client-go/alkira/peering_gateway_cxp.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/peering_gateway_cxp.go
@@ -8,18 +8,16 @@ import (
 )
 
 type PeeringGatewayCxp struct {
-	Name          string `json:"name"`
-	Description   string `json:"description,omitempty"`
-	Cxp           string `json:"cxp"`
-	CloudProvider string `json:"cloudProvider"`
-	CloudRegion   string `json:"cloudRegion"`
-	Segment       string `json:"segment"`
-	// SegmentId     string      `json:"segmentId"`
-	Id    json.Number `json:"id,omitempty"`    // response only
-	State string      `json:"state,omitempty"` // response only
+	Name          string      `json:"name"`
+	Description   string      `json:"description,omitempty"`
+	Cxp           string      `json:"cxp"`
+	CloudProvider string      `json:"cloudProvider"`
+	CloudRegion   string      `json:"cloudRegion"`
+	Segment       string      `json:"segment"`
+	Id            json.Number `json:"id,omitempty"`    // response only
+	State         string      `json:"state,omitempty"` // response only
 }
 
-// NewConnectorPeeringGatewayAwsTgwAttachment new peering gateway aws tgw attachment
 func NewPeeringGatewayCxp(ac *AlkiraClient) *AlkiraAPI[PeeringGatewayCxp] {
 	uri := fmt.Sprintf("%s/tenantnetworks/%s/cxp-peering-gateways", ac.URI, ac.TenantNetworkId)
 	api := &AlkiraAPI[PeeringGatewayCxp]{ac, uri, false}
