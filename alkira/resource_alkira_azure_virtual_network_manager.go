74547694259d7486476f439955c4020cc2079a7e
Asim Ali Khan <asim.ali_khan@alkira.net>
2024-06-13 01:20:15 +0530

    added create function and updated the wrapper struct

--- alkira/resource_alkira_azure_virtual_network_manager.go
+++ alkira/resource_alkira_azure_virtual_network_manager.go
@@ -3,20 +3,20 @@ package alkira
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/alkiranet/alkira-client-go/alkira"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
-func resourceAlkiraVirtualNetworkManagerAzure() *schema.Resource {
+func resourceAzureVirtualNetworkManager() *schema.Resource {
 	return &schema.Resource{
 		Description:   "Manager Virtual Network Manager for Azure.",
-		CreateContext: resourceAlkiraVirtualNetworkManagerAzureCreate,
-		UpdateContext: resourceAlkiraVirtualNetworkManagerAzureUpdate,
-		DeleteContext: resourceAlkiraVirtualNetworkManagerAzureDelete,
-		ReadContext:   resourceAlkiraVirtualNetworkManagerAzureRead,
+		CreateContext: resourceAzureVirtualNetworkManagerCreate,
+		UpdateContext: resourceAzureVirtualNetworkManagerUpdate,
+		DeleteContext: resourceAzureVirtualNetworkManagerDelete,
+		ReadContext:   resourceAzureVirtualNetworkManagerRead,
 		CustomizeDiff: func(ctx context.Context, d *schema.ResourceDiff, m interface{}) error {
 			client := m.(*alkira.AlkiraClient)
 
@@ -31,33 +31,68 @@ func resourceAlkiraVirtualNetworkManagerAzure() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
+
 		Schema: map[string]*schema.Schema{
-    "name":{
-    Description: "Name of the ",
-    }
-
-    },
-
+			"name": {
+				Description: "The name of the connector.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+		},
 	}
 
 }
 
-func resourceAlkiraVirtualNetworkManagerAzureCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+func resourceAzureVirtualNetworkManagerCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	// INIT
+	api := alkira.NewAzureVirtualNetworkManager(m.(*alkira.AlkiraClient))
+
+	request, err := generateAzureVirtualNetworkManagerRequest(d, m)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	resource, _, err, _ := api.Create(request)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(string(resource.Id))
+
+	// WAITING FOR THE STATE
+	state := resource.State
+	for state != "ACTIVE" {
+		resource, _, err := api.GetById(d.Id())
+
+		if err != nil {
+			return diag.Diagnostics{{
+				Severity: diag.Warning,
+				Summary:  "FAILED TO GET RESOURCE",
+				Detail:   fmt.Sprintf("%s", err),
+			}}
+		}
+
+		state = resource.State
+		time.Sleep(5 * time.Second)
+
+	}
+	return resourceAzureVirtualNetworkManagerRead(ctx, d, m)
+}
+
+func resourceAzureVirtualNetworkManagerUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	return nil
 }
 
-func resourceAlkiraVirtualNetworkManagerAzureUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+func resourceAzureVirtualNetworkManagerDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	return nil
 }
 
-func resourceAlkiraVirtualNetworkManagerAzureDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+func resourceAzureVirtualNetworkManagerRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	return nil
 }
 
-func resourceAlkiraVirtualNetworkManagerAzureRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	return nil
-}
-
-func generateVirtualNetworkManagerAzureRequest(d *schema.ResourceData, m interface{}) (*alkira.VirtualNetworkManagerAzure, error) {
+func generateAzureVirtualNetworkManagerRequest(d *schema.ResourceData, m interface{}) (*alkira.AzureVirtualNetworkManager, error) {
 	return nil, nil
 }
--- vendor/github.com/alkiranet/alkira-client-go/alkira/virtual_network_manager_azure.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/virtual_network_manager_azure.go
@@ -1,20 +1,23 @@
 package alkira
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
-type VirtualNetworkManagerAzure struct {
-	Name                 string   `json:"name"`
-	Region               string   `json:"region"`
-	SubscriptionId       string   `json:"subscriptionId"`
-	ResourceGroup        string   `json:"resourceGroup"`
-	Description          string   `json:"description,omitempty"`
-	CredentialsId        string   `json:"credentialsId"`
-	SubscriptionsInScope []string `json:"subscriptionsInScope"`
+type AzureVirtualNetworkManager struct {
+	Name                 string      `json:"name"`
+	Region               string      `json:"region"`
+	SubscriptionId       string      `json:"subscriptionId"`
+	ResourceGroup        string      `json:"resourceGroup"`
+	Description          string      `json:"description,omitempty"`
+	CredentialsId        string      `json:"credentialsId"`
+	Id                   json.Number `json:"id,omitempty"`
+	State                string      `json:"state,omitempty"`
+	SubscriptionsInScope []string    `json:"subscriptionsInScope"`
 }
 
-func NewVritualNetworkManagerAzure(ac *AlkiraClient) *AlkiraAPI[VirtualNetworkManagerAzure] {
+func NewAzureVirtualNetworkManager(ac *AlkiraClient) *AlkiraAPI[AzureVirtualNetworkManager] {
 	uri := fmt.Sprintf("%s/tenantnetworks/%s/azure-virtual-network-managers", ac.URI, ac.TenantNetworkId)
 	api := &AlkiraAPI[VirtualNetworkManagerAzure]{ac, uri, true}
 	return api
