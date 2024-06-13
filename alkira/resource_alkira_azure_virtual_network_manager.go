321a0242370bffe007c635a0ce7e2fd3dddd7946
Asim Ali Khan <asim.ali_khan@alkira.net>
2024-06-13 14:02:45 +0530

    added the read, update and delete functions

--- alkira/resource_alkira_azure_virtual_network_manager.go
+++ alkira/resource_alkira_azure_virtual_network_manager.go
@@ -34,9 +34,43 @@ func resourceAzureVirtualNetworkManager() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Description: "The name of the connector.",
+				Description: "The name of the Azure Virtual Network Manager.",
 				Type:        schema.TypeString,
 				Required:    true,
+			},
+			"region": {
+				Description: "The Azure Region where the Azure Virtual Network" +
+					" Manager is created. eg: `eastus`.",
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"subscription_id": {
+				Description: "The ID of the Azure Subscription account in which the" +
+					" Azure Virtual Network Manager is created.",
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"resource_group": {
+				Description: "The name of the Azure Resource Group in which the" +
+					" Azure Virtual Network Manager is created.",
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"Description": {
+				Description: "The description of the Azure Virtual Network Manager.",
+				Type:        schema.TypeString,
+				Optional:    true,
+			},
+			"credential_id": {
+				Description: "ID of credential managed by Credential Manager.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"subscriptions_in_scope": {
+				Description: "List IDs of Azure Subscription Accounts that will be" +
+					" managed by this Azure Virtual Network Manager.",
+				Type:     schema.TypeList,
+				Required: true,
 			},
 		},
 	}
@@ -82,17 +116,84 @@ func resourceAzureVirtualNetworkManagerCreate(ctx context.Context, d *schema.Res
 }
 
 func resourceAzureVirtualNetworkManagerUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	return nil
+	// CHECK FOR CHANGES IN ANY FIELD EXCEPT DESCRIPTION AND RETURN ERR.
+	if d.HasChange("description") {
+
+		// INIT
+		api := alkira.NewAzureVirtualNetworkManager(m.(*alkira.AlkiraClient))
+
+		request, err := generateAzureVirtualNetworkManagerRequest(d, m)
+
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		// UPDATE
+		_, err, _ = api.Update(d.Id(), request)
+
+		return nil
+	}
+
+	// RETURN ERROR IF ANY FIELD EXCEPT DESCRIPTION IS CHANGED.
+	return diag.Diagnostics{{
+		Severity: diag.Error,
+		Summary:  "INVALID UPDATE!",
+		Detail:   "ONLY THE DESCRIPTION FIELD CAN BE UPDATED.",
+	}}
 }
 
 func resourceAzureVirtualNetworkManagerDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	// INIT
+	api := alkira.NewAzureVirtualNetworkManager(m.(*alkira.AlkiraClient))
+
+	// DELETE
+	_, err, _ := api.Delete(d.Id())
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// SETTING THE ID TO EMPTY STRING
+	d.SetId("")
 	return nil
 }
 
 func resourceAzureVirtualNetworkManagerRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	// INIT
+	api := alkira.NewAzureVirtualNetworkManager(m.(*alkira.AlkiraClient))
+	resource, _, err := api.GetById(d.Id())
+
+	if err != nil {
+		return diag.Diagnostics{{
+			Severity: diag.Warning,
+			Summary:  "FAILED TO GET RESOURCE",
+			Detail:   fmt.Sprintf("%s", err),
+		}}
+	}
+
+	d.Set("name", resource.Name)
+	d.Set("region", resource.Region)
+	d.Set("subscription_id", resource.SubscriptionId)
+	d.Set("resource_group", resource.ResourceGroup)
+	d.Set("description", resource.Description)
+	d.Set("credential_id", resource.CredentialId)
+	d.Set("subscriptions_in_scope", resource.SubscriptionsInScope)
+
 	return nil
 }
 
 func generateAzureVirtualNetworkManagerRequest(d *schema.ResourceData, m interface{}) (*alkira.AzureVirtualNetworkManager, error) {
-	return nil, nil
+
+	request := &alkira.AzureVirtualNetworkManager{
+		Name:                 d.Get("name").(string),
+		Region:               d.Get("region").(string),
+		SubscriptionId:       d.Get("subscription_id").(string),
+		ResourceGroup:        d.Get("resource_group").(string),
+		Description:          d.Get("description").(string),
+		CredentialId:         d.Get("credential_id").(string),
+		SubscriptionsInScope: d.Get("subscriptions_in_scope").([]string),
+	}
+	return request, nil
 }
