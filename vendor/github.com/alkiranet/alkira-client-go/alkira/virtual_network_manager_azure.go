8e3c70e45a8c3662e8687f2e7a284fda4187dea7
Spike Wang <spike@alkira.com>
2024-07-01 22:25:17 -0700

    AK-46036 Add release notes for v1.2.6 and fix dependbot warnings (#320)
    + Add release notes for  v1.2.6.
    + Fix dependbot security alerts.
    + Update alkira-client-go to v1.50.0

---
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/virtual_network_manager_azure.go
@@ -0,0 +1,26 @@
+// Copyright (C) 2024 Alkira Inc. All Rights Reserved.
+package alkira
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type VirtualNetworkManagerAzure struct {
+	Name                 string      `json:"name"`
+	Region               string      `json:"region"`
+	SubscriptionId       string      `json:"subscriptionId"`
+	ResourceGroup        string      `json:"resourceGroup"`
+	Cxp                  string      `json:"cxp"`
+	Description          string      `json:"description,omitempty"`
+	CredentialId         string      `json:"credentialId"`
+	Id                   json.Number `json:"id,omitempty"`        // RESPONSE ONLY.
+	State                string      `json:"avnmState,omitempty"` // RESPONSE ONLY.
+	SubscriptionsInScope []string    `json:"subscriptionsInScope"`
+}
+
+func NewVirtualNetworkManagerAzure(ac *AlkiraClient) *AlkiraAPI[VirtualNetworkManagerAzure] {
+	uri := fmt.Sprintf("%s/tenantnetworks/%s/azure-virtual-network-managers", ac.URI, ac.TenantNetworkId)
+	api := &AlkiraAPI[VirtualNetworkManagerAzure]{ac, uri, true}
+	return api
+}
