fff7e1fab35a2682780a5f2a7d0c330edd43ce1b
Asim Ali Khan <asim.ali_khan@alkira.net>
2024-08-21 16:17:55 +0530

    add deprecation note to byoip data-source

--- alkira/provider.go
+++ alkira/provider.go
@@ -110,7 +110,7 @@ func Provider() *schema.Provider {
 		DataSourcesMap: map[string]*schema.Resource{
 			"alkira_billing_tag":                        dataSourceAlkiraBillingTag(),
 			"alkira_byoip_prefix":                       dataSourceAlkiraByoipPrefix(),
-			"alkira_byoip":                              dataSourceAlkiraByoipPrefix(),
+			"alkira_byoip":                              dataSourceAlkiraByoip(),
 			"alkira_credential":                         dataSourceAlkiraCredential(),
 			"alkira_connector_akamai_prolexic":          dataSourceAlkiraConnectorAkamaiProlexic(),
 			"alkira_connector_aruba_edge":               dataSourceAlkiraConnectorArubaEdge(),
