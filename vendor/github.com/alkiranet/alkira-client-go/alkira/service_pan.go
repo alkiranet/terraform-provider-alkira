eead8a9709bbb3c8e97071a24102117547ebd817
Spike Wang <spike@alkira.com>
2024-09-26 11:29:23 -0700

    AK-48229 Update alkira-client-go to v1.51.1
    Update alkira-client-go to v1.51.1 to revert some new changes that
    are not ready.

--- vendor/github.com/alkiranet/alkira-client-go/alkira/service_pan.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/service_pan.go
@@ -67,7 +67,6 @@ type ServicePanInstance struct {
 	Id                          int                                          `json:"id,omitempty"`
 	MasterKeyEnabled            bool                                         `json:"masterKeyEnabled,omitempty"`
 	Name                        string                                       `json:"name"`
-	TrafficEnabled              bool                                         `json:"trafficEnabled"`
 }
 
 // NewServicePan new service pan
