1ea57232bec5171e7423e9ddbe9ed9a6072516ae
Asim Ali Khan <asim.ali_khan@alkira.net>
2024-09-27 12:37:53 +0530

    add example for f5 vserver and update docs

---
+++ examples/resources/alkira_f5_vserver_endpoint/resource.tf
@@ -0,0 +1,12 @@
+resource "alkira_f5_vserver" "example_vserver" {
+  name                    = "example"
+  f5_service_id           = alkira_service_f5_lb.example_lb.id
+  f5_service_instance_ids = [alkira_service_f5_lb.example_lb.instances[0].id]
+  type                    = "ELB"
+  segment_id              = alkira_segment.example_segment.id
+  fqdn_prefix             = "example_prefix"
+  protocol                = "UDP"
+  port_ranges             = ["-1"]
+  snat                    = "AUTOMAP"
+
+}
