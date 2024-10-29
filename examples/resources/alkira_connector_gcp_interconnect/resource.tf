178407bf452a4799b7540c0bca6bad8e476c413f
Asim Ali Khan <asim.ali_khan@alkira.net>
2024-10-29 23:54:44 +0530

    add examples and docs

---
+++ examples/resources/alkira_connector_gcp_interconnect/resource.tf
@@ -0,0 +1,25 @@
+resource "alkira_connector_gcp_interconnect" "example_gcp_interconnect" {
+  name = "example_gcp_interconnect"
+  size = "SMALL"
+  description = "example connector"
+  cxp = "US-WEST"
+  group = alkira_group.group1.name
+  tunnel_protocol = "IPSEC"
+  instances ={
+    name = "instance1"
+    edge_availibility_domain = "AVAILIBILITY_DOMAIN_1"
+    candidate_subnets = ["10.30.0.0/24"]
+    customer_asn = 56009
+    bgp_auth_key  = "key"
+  }
+  segment_options {
+    segment_id = alkira_segment.segment1.id
+    instance_name = "instance1"
+    advertise_on_prem_routes = true
+    disable_internet_exit = false
+    customer_gateways {
+      loopback_ip = "192.192.192.192"
+      tunnel_count = 2
+    }
+  }
+}
