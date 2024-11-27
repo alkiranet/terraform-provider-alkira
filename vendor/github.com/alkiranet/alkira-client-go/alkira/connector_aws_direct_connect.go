8f6faa593949eb931bb56c6f6935258bd54391bc
Spike Wang <spike@alkira.com>
2024-11-26 21:22:53 -0800

    Move alkira-client-go to v1.52.2

--- vendor/github.com/alkiranet/alkira-client-go/alkira/connector_aws_direct_connect.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/connector_aws_direct_connect.go
@@ -8,49 +8,49 @@ import (
 )
 
 type ConnectorAwsDirectConnectSegmentOption struct {
-	SegmentName                      string  `json:"segmentName"`
-	CustomerAsn                      int     `json:"customerAsn"`
-	CustomerLoopbackIp               string  `json:"customerLoopbackIp,omitempty"`
-	AlkLoopbackIp1                   string  `json:"alkLoopbackIp1,omitempty"`
-	AlkLoopbackIp2                   string  `json:"alkLoopbackIp2,omitempty"`
-	LoopbackSubnet                   string  `json:"loopbackSubnet"`
-	AdvertiseOnPremRoutes            bool `json:"advertiseOnPremRoutes"`
-	DisableInternetExit              bool `json:"disableInternetExit"`
-	NumOfCustomerLoopbackIps         int     `json:"numOfCustomerLoopbackIps,omitempty"`
-	TunnelCountPerCustomerLoopbackIp int     `json:"tunnelCountPerCustomerLoopbackIp,omitempty"`
+	SegmentName                      string `json:"segmentName"`
+	CustomerAsn                      int    `json:"customerAsn"`
+	CustomerLoopbackIp               string `json:"customerLoopbackIp,omitempty"`
+	AlkLoopbackIp1                   string `json:"alkLoopbackIp1,omitempty"`
+	AlkLoopbackIp2                   string `json:"alkLoopbackIp2,omitempty"`
+	LoopbackSubnet                   string `json:"loopbackSubnet"`
+	AdvertiseOnPremRoutes            bool   `json:"advertiseOnPremRoutes"`
+	DisableInternetExit              bool   `json:"disableInternetExit"`
+	NumOfCustomerLoopbackIps         int    `json:"numOfCustomerLoopbackIps,omitempty"`
+	TunnelCountPerCustomerLoopbackIp int    `json:"tunnelCountPerCustomerLoopbackIp,omitempty"`
 }
 
 type ConnectorAwsDirectConnectInstance struct {
-	Id                int              `json:"id,omitempty"` // response only
-	Name              string           `json:"name"`
-	ConnectionId      string           `json:"connectionId"`
-	DcGatewayAsn      int              `json:"dcGatewayAsn"`
-	UnderlayAsn       int              `json:"underlayAsn"`
-	UnderlayPrefix    string           `json:"underlayPrefix"`
-	AwsUnderlayIp     string           `json:"awsUnderlayIp,omitempty"`
-	OnPremUnderlayIp  string           `json:"onPremUnderlayIp,omitempty"`
-	BgpAuthKey        string           `json:"bgpAuthKey,omitempty"`
-	BgpAuthKeyAlkira  string           `json:"bgpAuthKeyAlkira,omitempty"`
-	Vlan              int              `json:"vlan"`
-	CustomerRegion    string           `json:"customerRegion"`
-	CredentialId      string           `json:"credentialId"`
-	GatewayMacAddress string           `json:"gatewayMacAddress,omitempty"`
-	Vni               int              `json:"vni,omitempty"`
+	Id                int                                      `json:"id,omitempty"` // response only
+	Name              string                                   `json:"name"`
+	ConnectionId      string                                   `json:"connectionId"`
+	DcGatewayAsn      int                                      `json:"dcGatewayAsn"`
+	UnderlayAsn       int                                      `json:"underlayAsn"`
+	UnderlayPrefix    string                                   `json:"underlayPrefix"`
+	AwsUnderlayIp     string                                   `json:"awsUnderlayIp,omitempty"`
+	OnPremUnderlayIp  string                                   `json:"onPremUnderlayIp,omitempty"`
+	BgpAuthKey        string                                   `json:"bgpAuthKey,omitempty"`
+	BgpAuthKeyAlkira  string                                   `json:"bgpAuthKeyAlkira,omitempty"`
+	Vlan              int                                      `json:"vlan"`
+	CustomerRegion    string                                   `json:"customerRegion"`
+	CredentialId      string                                   `json:"credentialId"`
+	GatewayMacAddress string                                   `json:"gatewayMacAddress,omitempty"`
+	Vni               int                                      `json:"vni,omitempty"`
 	SegmentOptions    []ConnectorAwsDirectConnectSegmentOption `json:"segmentOptions"`
 }
 
 type ConnectorAwsDirectConnect struct {
-	Id             json.Number `json:"id,omitempty"` // response only
-	Name           string      `json:"name"`
-	Description    string      `json:"description,omitempty"`
-	BillingTags    []int       `json:"billingTags"`
-	Cxp            string      `json:"cxp"`
-	Enabled        bool        `json:"enabled"`
-	Group          string      `json:"group,omitempty"`
-	ImplicitGroupId   int              `json:"implicitGroupId,omitempty"` // response only
-	Size           string      `json:"size"`
-	TunnelProtocol string      `json:"tunnelProtocol"`
-	Instances      []ConnectorAwsDirectConnectInstance `json:"instances"`
+	Id              json.Number                         `json:"id,omitempty"` // response only
+	Name            string                              `json:"name"`
+	Description     string                              `json:"description,omitempty"`
+	BillingTags     []int                               `json:"billingTags"`
+	Cxp             string                              `json:"cxp"`
+	Enabled         bool                                `json:"enabled"`
+	Group           string                              `json:"group,omitempty"`
+	ImplicitGroupId int                                 `json:"implicitGroupId,omitempty"` // response only
+	Size            string                              `json:"size"`
+	TunnelProtocol  string                              `json:"tunnelProtocol"`
+	Instances       []ConnectorAwsDirectConnectInstance `json:"instances"`
 }
 
 // NewConnectorAwsDirectConnect new connector-aws-direct-connect
