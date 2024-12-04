5ea71a646c6adf1524bb947a5944260658513ef3
Asim Ali Khan <146973802+asim-ak@users.noreply.github.com>
2024-12-04 23:06:24 +0530

    AK-50656 Update GCP Interconnect Schema. (#345)
    Move segment_options to instance block in connector-gcp-interconnect

--- examples/resources/alkira_connector_gcp_interconnect/resource1.tf
+++ examples/resources/alkira_connector_gcp_interconnect/resource1.tf
@@ -11,6 +11,14 @@ resource "alkira_connector_gcp_interconnect" "example_gcp_interconnect_1" {
     edge_availability_domain = "AVAILABILITY_DOMAIN_1"
     customer_asn             = 56009
     bgp_auth_key             = "key"
+    segment_options {
+      segment_id               = alkira_segment.segment1.id
+      advertise_on_prem_routes = true
+      advertise_default_route = false
+      customer_gateways {
+        tunnel_count = 2
+      }
+    }
   }
 
   instances {
@@ -18,17 +26,13 @@ resource "alkira_connector_gcp_interconnect" "example_gcp_interconnect_1" {
     edge_availability_domain = "AVAILABILITY_DOMAIN_1"
     customer_asn             = 56010
     bgp_auth_key             = "key_2"
-  }
-  segment_options {
-    segment_id               = alkira_segment.segment1.id
-    instance_name            = "instance1"
-    advertise_on_prem_routes = true
-    disable_internet_exit    = false
-    customer_gateways {
-      tunnel_count = 2
-    }
-    customer_gateways {
-      tunnel_count = 1
+    segment_options {
+      segment_id               = alkira_segment.segment1.id
+      advertise_on_prem_routes = true
+      advertise_default_route = false
+      customer_gateways {
+        tunnel_count = 2
+      }
     }
   }
 }
--- examples/resources/alkira_connector_gcp_interconnect/resource3.tf
+++ examples/resources/alkira_connector_gcp_interconnect/resource3.tf
@@ -1,15 +1,38 @@
-resource "alkira_connector_gcp_interconnect" "example_gcp_interconnect_2" {
-  name              = "example_gcp_interconnect_1"
+resource "alkira_connector_gcp_interconnect" "example_gcp_interconnect_3" {
+  name              = "example_gcp_interconnect_3"
   size              = "LARGE"
   description       = "example connector with multiple segment options"
   cxp               = "US-WEST"
   group             = alkira_group.group1.name
-  tunnel_protocol   = "IPSEC"
+  tunnel_protocol   = "GRE"
   loopback_prefixes = ["10.40.0.0/24"]
   instances {
     name                     = "instance1"
     edge_availability_domain = "AVAILABILITY_DOMAIN_1"
     customer_asn             = 56009
+    bgp_auth_key             = "key"
+    segment_options {
+      segment_id               = alkira_segment.segment1.id
+      advertise_on_prem_routes = true
+      advertise_default_route = false
+      customer_gateways {
+        tunnel_count = 2
+      }
+    }
+    segment_options {
+      segment_id               = alkira_segment.segment2.id
+      advertise_on_prem_routes = true
+      advertise_default_route = false
+      customer_gateways {
+        tunnel_count = 2
+      }
+    }
+  }
+
+  instances {
+    name                     = "instance2"
+    edge_availability_domain = "AVAILABILITY_DOMAIN_1"
+    customer_asn             = 56011
     bgp_auth_key             = "key"
     segment_options {
       segment_id               = alkira_segment.segment1.id
