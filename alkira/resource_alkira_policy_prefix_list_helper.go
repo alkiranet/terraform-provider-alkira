602813e569f064d995cd0f74cdfdee360e823757
Spike Wang <spike@alkira.com>
2025-03-05 21:01:03 -0800

    AK-51400 Fix read of prefix_range blocks in policy-prefix-list
    Add read function for prefix_range blocks in policy-prefix-list.

--- alkira/resource_alkira_policy_prefix_list_helper.go
+++ alkira/resource_alkira_policy_prefix_list_helper.go
@@ -4,8 +4,27 @@ import (
 	"log"
 
 	"github.com/alkiranet/alkira-client-go/alkira"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+// setPrefixRange set "prefix_range" blocks from API response
+func setPrefixRanges(d *schema.ResourceData, r []alkira.PolicyPrefixListRange) {
+	var prefixRanges []map[string]interface{}
+
+	for _, rng := range r {
+		prefixRange := map[string]interface{}{
+			"prefix": rng.Prefix,
+			"le":     rng.Le,
+			"ge":     rng.Ge,
+		}
+		prefixRanges = append(prefixRanges, prefixRange)
+	}
+
+	d.Set("prefix_range", prefixRanges)
+}
+
+// expandPrefixListPrefixRanges expand block "prefix_range" to
+// construct payload
 func expandPrefixListPrefixRanges(in []interface{}) ([]alkira.PolicyPrefixListRange, error) {
 
 	if in == nil || len(in) == 0 {
