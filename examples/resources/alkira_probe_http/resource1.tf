48808989c745c04f20bb13fa3d813821dd7372ec
Asim Ali Khan <146973802+asim-ak@users.noreply.github.com>
2025-05-05 22:54:17 +0530

    AK-53137 Add initial support for probe resources  (#372)
    Add initial support for new probe resources.
    
    ---------
    
    Co-authored-by: Spike Wang <spike@alkira.com>

---
+++ examples/resources/alkira_probe_http/resource1.tf
@@ -0,0 +1,32 @@
+resource "alkira_probe_http" "advanced_http" {
+  name    = "http-with-validators"
+  enabled = true
+
+  network_entity {
+    type = "INTERNET_APPLICATION"
+    id   = alkira_internet_application.example_application.id
+  }
+
+  uri = "www.alkira.net/api/health"
+
+  headers = {
+    "Authorization" = "Bearer token123"
+    "Content-Type"  = "application/json"
+    "Accept"        = "application/json"
+  }
+
+  validators {
+    type        = "STATUS_CODE"
+    status_code = "200-299"
+  }
+
+  validators {
+    type  = "RESPONSE_BODY"
+    regex = ".*status.*:.*OK.*"
+  }
+
+  failure_threshold = 3
+  success_threshold = 2
+  period_seconds    = 30
+  timeout_seconds   = 10
+}
