dc5998fe38fb2029ca464409ea22d7c004a7d3cb
Spike Wang <spike@alkira.com>
2025-07-10 14:55:29 -0700

    AK-57879 Make release v1.3.2
    Update documentation and add release note for v1.3.2

--- examples/resources/alkira_connector_aws_vpc/resource.tf
+++ examples/resources/alkira_connector_aws_vpc/resource.tf
@@ -1,13 +1,117 @@
-resource "alkira_connector_aws_vpc" "connector" {
-  name           = "connector-test"
-  vpc_id         = "your_vpc_id"
-
-  aws_account_id = "your_aws_account_id"
+# Basic AWS VPC Connector Example
+resource "alkira_connector_aws_vpc" "basic" {
+  name           = "aws-vpc-basic"
+  description    = "Basic AWS VPC connector example"
+  vpc_id         = "vpc-12345678"
+  aws_account_id = "123456789012"
   aws_region     = "us-east-1"
-
   credential_id  = alkira_credential_aws_vpc.account1.id
   cxp            = "US-WEST"
   group          = alkira_group.group1.name
   segment_id     = alkira_segment.segment1.id
   size           = "SMALL"
+  enabled        = true
+}
+
+# Advanced AWS VPC Connector with High Availability
+resource "alkira_connector_aws_vpc" "advanced" {
+  name            = "aws-vpc-advanced"
+  description     = "Advanced AWS VPC connector with high availability"
+  vpc_id          = "vpc-87654321"
+  aws_account_id  = "123456789012"
+  aws_region      = "us-east-1"
+  credential_id   = alkira_credential_aws_vpc.account1.id
+  cxp             = "US-WEST"
+  failover_cxps   = ["US-EAST"]
+  group           = alkira_group.group1.name
+  segment_id      = alkira_segment.segment1.id
+  size            = "MEDIUM"
+  enabled         = true
+  billing_tag_ids = [alkira_billing_tag.tag1.id]
+
+  # VPC CIDR blocks for routing
+  vpc_cidr = [
+    "10.0.0.0/16",
+    "10.1.0.0/16"
+  ]
+
+  # TGW Connect for high performance
+  tgw_connect_enabled = true
+
+  # TGW attachment configuration
+  tgw_attachment {
+    subnet_id = "subnet-12345678"
+    az        = "us-east-1a"
+  }
+
+  tgw_attachment {
+    subnet_id = "subnet-87654321"
+    az        = "us-east-1b"
+  }
+
+  # Enable direct inter-VPC communication
+  direct_inter_vpc_communication_enabled = true
+  direct_inter_vpc_communication_group   = "production-vpcs"
+}
+
+# VPC Connector with Subnet-based Routing
+resource "alkira_connector_aws_vpc" "subnet_routing" {
+  name            = "aws-vpc-subnet-routing"
+  description     = "AWS VPC connector with subnet-based routing"
+  vpc_id          = "vpc-abcdef12"
+  aws_account_id  = "123456789012"
+  aws_region      = "us-west-2"
+  credential_id   = alkira_credential_aws_vpc.account1.id
+  cxp             = "US-WEST"
+  group           = alkira_group.group1.name
+  segment_id      = alkira_segment.segment1.id
+  size            = "SMALL"
+  enabled         = true
+  billing_tag_ids = [alkira_billing_tag.tag1.id]
+
+  # Use specific subnets instead of VPC CIDR
+  vpc_subnet {
+    id   = "subnet-production1"
+    cidr = "10.0.1.0/24"
+  }
+
+  vpc_subnet {
+    id   = "subnet-production2"
+    cidr = "10.0.2.0/24"
+  }
+
+  # Route table configuration
+  vpc_route_table {
+    id      = "rtb-12345678"
+    options = "ADVERTISE_DEFAULT_ROUTE"
+  }
+
+  vpc_route_table {
+    id      = "rtb-87654321"
+    options = "ADVERTISE_CUSTOM_PREFIX"
+  }
+}
+
+# Required supporting resources for the examples
+resource "alkira_billing_tag" "tag1" {
+  name        = "aws-vpc-connector-tag"
+  description = "Billing tag for AWS VPC connectors"
+}
+
+resource "alkira_credential_aws_vpc" "account1" {
+  name           = "aws-account-credentials"
+  aws_account_id = "123456789012"
+  aws_access_key = "your_access_key"
+  aws_secret_key = "your_secret_key"
+}
+
+resource "alkira_group" "group1" {
+  name        = "aws-vpc-group"
+  description = "Group for AWS VPC connectors"
+}
+
+resource "alkira_segment" "segment1" {
+  name  = "production-segment"
+  asn   = "65001"
+  cidrs = ["10.0.0.0/8"]
 }
--- examples/resources/alkira_policy_rule/resource.tf
+++ examples/resources/alkira_policy_rule/resource.tf
@@ -1,6 +1,7 @@
-resource "alkira_policy_rule" "rule" {
-  name          = "test-rule"
-  description   = "Test Rule"
+# Basic Policy Rule Example
+resource "alkira_policy_rule" "basic" {
+  name          = "basic-drop-rule"
+  description   = "Basic policy rule to drop traffic"
   src_ip        = "any"
   dst_ip        = "172.16.0.0/16"
   dscp          = "any"
@@ -8,4 +9,55 @@ resource "alkira_policy_rule" "rule" {
   src_ports     = ["any"]
   dst_ports     = ["any"]
   rule_action   = "DROP"
+}
+
+# Advanced Policy Rule with Prefix Lists
+resource "alkira_policy_rule" "advanced" {
+  name                  = "advanced-service-rule"
+  description           = "Advanced policy rule with service chaining"
+  src_prefix_list_id    = alkira_list_global_cidr.internal_networks.id
+  dst_prefix_list_id    = alkira_list_global_cidr.dmz_networks.id
+  dscp                  = "any"
+  protocol              = "tcp"
+  src_ports             = ["any"]
+  dst_ports             = ["80", "443"]
+  rule_action           = "ALLOW"
+
+  # Service chaining
+  rule_action_service_types = ["FIREWALL", "IDS"]
+  rule_action_service_ids   = [alkira_service_pan.firewall.id, alkira_service_checkpoint.ids.id]
+
+  # Flow collection
+  rule_action_flow_collector_ids = [alkira_flow_collector.security.id]
+}
+
+# Policy Rule with Internet Application
+resource "alkira_policy_rule" "internet_app" {
+  name                    = "internet-app-rule"
+  description             = "Policy rule for internet application access"
+  src_ip                  = "10.0.0.0/8"
+  internet_application_id = alkira_internet_application.office365.id
+  dscp                    = "any"
+  protocol                = "tcp"
+  src_ports               = ["any"]
+  dst_ports               = ["443"]
+  rule_action             = "ALLOW"
+}
+
+# Policy Rule with Application IDs
+resource "alkira_policy_rule" "application_based" {
+  name           = "application-based-rule"
+  description    = "Policy rule based on application identification"
+  src_ip         = "any"
+  dst_ip         = "any"
+  dscp           = "any"
+  protocol       = "any"
+  src_ports      = ["any"]
+  dst_ports      = ["any"]
+  application_ids = ["HTTP", "HTTPS", "SSH"]
+  rule_action    = "ALLOW"
+
+  # Route through security services
+  rule_action_service_types = ["FIREWALL"]
+  rule_action_service_ids   = [alkira_service_pan.firewall.id]
 }
