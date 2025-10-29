bab35eb68618e23c167ab25573ff46b92b7230fb
Asim Ali Khan <146973802+asim-ak@users.noreply.github.com>
2025-10-29 06:32:54 +0530

    AK-54913 - add controller scale options resource (#407)
    * feat(provider): add controller scale options resource
    
    Add new alkira_controller_scale_options resource to manage controller scale options for Alkira services and connectors. This includes full CRUD operations, schema validation, and comprehensive documentation with examples.
    
    * feat(provider): add additional_nodes field to controller scale options
    
    Add support for configuring additional nodes in the controller scale options
    resource.
    
    * AK-54913: fix all required and computed field values
    
    * AK-54913: fix review comments
    
    * AK-54913: update documentation
    
    ---------
    
    Co-authored-by: shrey <shrey.pasari@alkira.net>
    Co-authored-by: shrey-pasari <168513003+shrey-pasari@users.noreply.github.com>

---
+++ examples/resources/alkira_controller_scale_options/resource1.tf
@@ -0,0 +1,10 @@
+resource "alkira_controller_scale_options" "another_example" {
+  name        = "another-scale-options"
+  description = "Another example description"
+  entity_id   = alkira_connector_aws_vpc.id
+  entity_type = "CONNECTOR"
+  segment_scale_options {
+    additional_nodes            = 2
+    segment_id                  = alkira_segment.example.id
+  }
+}
