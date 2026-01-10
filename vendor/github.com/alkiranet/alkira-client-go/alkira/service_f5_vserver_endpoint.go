213e3065f25d54c9a275d97aad4cf2b66fbb9e10
AK-Benjamin <157636409+AK-Benjamin@users.noreply.github.com>
2026-01-09 16:12:40 -0800

    Reapply "AK-60435: Terraform Support for F5 ILB (#412)" (#417) (#437)
    This reverts commit cac88288cb01df3a9bea1801694c41e144a75513.

--- vendor/github.com/alkiranet/alkira-client-go/alkira/service_f5_vserver_endpoint.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/service_f5_vserver_endpoint.go
@@ -17,26 +17,11 @@ type F5vServerEndpoint struct {
 	F5ServiceInstanceIds []int                          `json:"f5ServiceInstanceIds,omitempty"`
 	PortRanges           []string                       `json:"portRanges,omitempty"`
 	DestinationEndpoints *F5VServerDestinationEndpoints `json:"destinationEndpoints,omitempty"`
-	Metadata             *F5VServerEndpointMetadata     `json:"metadata,omitempty"` // RESPONSE ONLY
 }
 
 type F5VServerDestinationEndpoints struct {
 	PortRanges  []string `json:"portRanges"`
 	IpAddresses []string `json:"ipAddresses"`
-}
-
-type F5VServerEndpointInstanceMetadata struct {
-	ElasticIp     string `json:"elasticIp,omitempty"`
-	SecondaryIp   string `json:"secondaryIp,omitempty"`
-	RouteDomainId int64  `json:"routeDomainId,omitempty"`
-	SegmentId     int64  `json:"segmentId,omitempty"`
-	Vlan          string `json:"vlan,omitempty"`
-	EcmpPoolName  string `json:"ecmpPoolName,omitempty"`
-	VirtualIp     string `json:"virtualIp,omitempty"`
-}
-
-type F5VServerEndpointMetadata struct {
-	InstanceToMetadata map[string]F5VServerEndpointInstanceMetadata `json:"instanceToMetadata,omitempty"`
 }
 
 func NewF5vServerEndpoint(ac *AlkiraClient) *AlkiraAPI[F5vServerEndpoint] {
