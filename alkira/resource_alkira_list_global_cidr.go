1ea1fad5692a413c4814d8f74c2698f075a5a9b6
Asim Ali Khan <146973802+asim-ak@users.noreply.github.com>
2026-01-29 21:49:54 +0530

    Update Global CIDR List constraints in documentation (#449)
    - values field: Document that only 1 CIDR is allowed per list
       - tags field: Clarify that only one service type is allowed

--- alkira/resource_alkira_list_global_cidr.go
+++ alkira/resource_alkira_list_global_cidr.go
@@ -54,16 +54,18 @@ func resourceAlkiraListGlobalCidr() *schema.Resource {
 				Computed:    true,
 			},
 			"values": {
-				Description: "A list of CIDRs, The CIDR must be `/24` and a " +
-					"subnet of the following: `10.0.0.0/18`, `172.16.0.0/12`, " +
-					"`192.168.0.0/16`, `100.64.0.0/10`.",
+				Description: "CIDR prefixes for the Global CIDR List. " +
+					"The CIDR must be `/24` and a subnet of the following: " +
+					"`10.0.0.0/18`, `172.16.0.0/12`, `192.168.0.0/16`, " +
+					"`100.64.0.0/10`. Currently limited to 1 CIDR per list.",
 				Type:     schema.TypeList,
 				Required: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 			"tags": {
-				Description: "Tag of associated service types." +
-					"Can be one of `INFOBLOX`,`CHKPFW`, `CISCO_FTDV_FW`, `BLUECAT` or `F5LB`.",
+				Description: "Service type that can use this Global CIDR List. " +
+					"Only one service type is allowed. Can be one of: " +
+					"`INFOBLOX`, `CHKPFW`, `CISCO_FTDV_FW`, `BLUECAT`, or `F5LB`.",
 				Type:     schema.TypeSet,
 				Optional: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
