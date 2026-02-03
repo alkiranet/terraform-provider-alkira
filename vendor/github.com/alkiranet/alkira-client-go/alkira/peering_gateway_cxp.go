11169008ddf23b8842dec6b55ec2a9058876b198
benjamin <benjamin.kim@alkira.net>
2026-02-02 16:19:43 -0800

    AK-62451: Update CXP Peering Gateway for 3rd Party VNET Conector

--- vendor/github.com/alkiranet/alkira-client-go/alkira/peering_gateway_cxp.go
+++ vendor/github.com/alkiranet/alkira-client-go/alkira/peering_gateway_cxp.go
@@ -7,15 +7,25 @@ import (
 	"fmt"
 )
 
+type PeeringGatewayCxpMetadata struct {
+	IlbIpAddress   string `json:"ilbIpAddress,omitempty"`
+	GuestEmail     string `json:"guestEmail,omitempty"`
+	VnetResourceId string `json:"vnetResourceId,omitempty"`
+	VnetName       string `json:"vnetName,omitempty"`
+	SubscriptionId string `json:"subscriptionId,omitempty"`
+	ResourceGroup  string `json:"resourceGroup,omitempty"`
+}
+
 type PeeringGatewayCxp struct {
-	Name          string      `json:"name"`
-	Description   string      `json:"description,omitempty"`
-	Cxp           string      `json:"cxp"`
-	CloudProvider string      `json:"cloudProvider"`
-	CloudRegion   string      `json:"cloudRegion"`
-	Segment       string      `json:"segment"`
-	Id            json.Number `json:"id,omitempty"`    // response only
-	State         string      `json:"state,omitempty"` // response only
+	Name          string                      `json:"name"`
+	Description   string                      `json:"description,omitempty"`
+	Cxp           string                      `json:"cxp"`
+	CloudProvider string                      `json:"cloudProvider"`
+	CloudRegion   string                      `json:"cloudRegion"`
+	Segment       string                      `json:"segment"`
+	Id            json.Number                 `json:"id,omitempty"`       // response only
+	State         string                      `json:"state,omitempty"`    // response only
+	Metadata      *PeeringGatewayCxpMetadata  `json:"metadata,omitempty"` // response only, available when state is ACTIVE
 }
 
 func NewPeeringGatewayCxp(ac *AlkiraClient) *AlkiraAPI[PeeringGatewayCxp] {
