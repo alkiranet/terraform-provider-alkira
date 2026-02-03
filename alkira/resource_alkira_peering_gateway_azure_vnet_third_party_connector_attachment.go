a3f6ee12f0a0540174ebf1c9ce276c045706b0eb
benjamin <benjamin.kim@alkira.net>
2026-02-03 09:18:52 -0800

    update delete

--- alkira/resource_alkira_peering_gateway_azure_vnet_third_party_connector_attachment.go
+++ alkira/resource_alkira_peering_gateway_azure_vnet_third_party_connector_attachment.go
@@ -109,7 +109,13 @@ func resourcePeeringGatewayAzureVnetThirdPartyConnectorAttachmentDelete(ctx cont
 
 	_, err, _, _ := api.Delete(d.Id())
 	if err != nil {
-		return diag.FromErr(err)
+		// Terraform may not print "with <resource address>" for destroys of objects
+		// that are no longer in configuration, so include identifying context here.
+		name, _ := d.GetOk("name")
+		if nameStr, ok := name.(string); ok && nameStr != "" {
+			return diag.FromErr(fmt.Errorf("%w alkira_peering_gateway_azure_vnet_third_party_connector_attachment (name=%q id=%s)", err, nameStr, d.Id()))
+		}
+		return diag.FromErr(fmt.Errorf("%w alkira_peering_gateway_azure_vnet_third_party_connector_attachment (id=%s)", err, d.Id()))
 	}
 
 	d.SetId("")
