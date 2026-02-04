748a9b1735a3e525dcb6652b30aa22eabde27123
AK-Benjamin <157636409+AK-Benjamin@users.noreply.github.com>
2026-02-04 07:28:05 -0800

    AK-62451: Update CXP Peering Gateway for 3rd Party VNET Conector (#451)

--- alkira/data_source_alkira_peering_gateway_cxp.go
+++ alkira/data_source_alkira_peering_gateway_cxp.go
@@ -1,6 +1,8 @@
 package alkira
 
 import (
+	"strconv"
+
 	"github.com/alkiranet/alkira-client-go/alkira"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -8,15 +10,91 @@ import (
 func dataSourceAlkiraPeeringGatewayCxp() *schema.Resource {
 	return &schema.Resource{
 		Description: "This data source allows to retrieve an existing " +
-			"Cxp Peering Gateway by its name.",
+			"Cxp Peering Gateway by its name or ID.",
 
 		Read: dataSourceAlkiraPeeringGatewayCxpRead,
 
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Description: "The name of the resource.",
+				Description:  "The name of the resource.",
+				Type:         schema.TypeString,
+				Optional:     true,
+				ExactlyOneOf: []string{"name", "id"},
+			},
+			"id": {
+				Description:  "The ID of the resource.",
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ExactlyOneOf: []string{"name", "id"},
+			},
+			"description": {
+				Description: "Description of the resource.",
 				Type:        schema.TypeString,
-				Required:    true,
+				Computed:    true,
+			},
+			"cxp": {
+				Description: "The CXP to which the Gateway is attached.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"cloud_provider": {
+				Description: "The cloud provider where this resource is created.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"cloud_region": {
+				Description: "The region of the specified cloud provider.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"segment_id": {
+				Description: "The ID of the segment that is associated with the resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"state": {
+				Description: "The state of the resource.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"metadata": {
+				Description: "Metadata information available once the Peering Gateway is in ACTIVE state.",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ilb_ip_address": {
+							Description: "Internal Load Balancer IP Address.",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						"guest_email": {
+							Description: "Guest email address.",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						"vnet_resource_id": {
+							Description: "Azure VNET Resource ID associated with the ATH.",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						"vnet_name": {
+							Description: "Azure VNET name associated with the ATH.",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						"subscription_id": {
+							Description: "Azure Subscription ID.",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						"resource_group": {
+							Description: "Azure Resource Group.",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+					},
+				},
 			},
 		},
 	}
@@ -25,12 +103,52 @@ func dataSourceAlkiraPeeringGatewayCxp() *schema.Resource {
 func dataSourceAlkiraPeeringGatewayCxpRead(d *schema.ResourceData, m interface{}) error {
 	api := alkira.NewPeeringGatewayCxp(m.(*alkira.AlkiraClient))
 
-	resource, _, err := api.GetByName(d.Get("name").(string))
+	var resource *alkira.PeeringGatewayCxp
+	var err error
+
+	// Lookup by name or ID
+	if name, ok := d.GetOk("name"); ok {
+		resource, _, err = api.GetByName(name.(string))
+		if err != nil {
+			return err
+		}
+	} else if id, ok := d.GetOk("id"); ok {
+		idStr := strconv.Itoa(id.(int))
+		resource, _, err = api.GetById(idStr)
+		if err != nil {
+			return err
+		}
+	}
+
+	d.SetId(string(resource.Id))
+	d.Set("name", resource.Name)
+
+	segmentId, err := getSegmentIdByName(resource.Segment, m)
 	if err != nil {
 		return err
 	}
 
-	d.SetId(string(resource.Id))
+	d.Set("description", resource.Description)
+	d.Set("cxp", resource.Cxp)
+	d.Set("cloud_provider", resource.CloudProvider)
+	d.Set("cloud_region", resource.CloudRegion)
+	d.Set("segment_id", segmentId)
+	d.Set("state", resource.State)
+
+	// Set metadata if available (only populated when state is ACTIVE)
+	if resource.Metadata != nil {
+		metadata := []interface{}{
+			map[string]interface{}{
+				"ilb_ip_address":   resource.Metadata.IlbIpAddress,
+				"guest_email":      resource.Metadata.GuestEmail,
+				"vnet_resource_id": resource.Metadata.VnetResourceId,
+				"vnet_name":        resource.Metadata.VnetName,
+				"subscription_id":  resource.Metadata.SubscriptionId,
+				"resource_group":   resource.Metadata.ResourceGroup,
+			},
+		}
+		d.Set("metadata", metadata)
+	}
 
 	return nil
 }
