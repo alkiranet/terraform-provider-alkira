c99b6722679b1d970665319bd7facbc110d91a34
Rohan Pillai <rohan@alkira.net>
2026-02-24 23:13:16 -0800

    AK-64758 Allow IP Reservation#prefix to be set by the backend

--- examples/resources/alkira_ip_reservation/resource.tf
+++ examples/resources/alkira_ip_reservation/resource.tf
@@ -1,4 +1,5 @@
-resource "alkira_ip_reservation" "test" {
+# With an explicit prefix
+resource "alkira_ip_reservation" "explicit_prefix" {
   name                = "test"
   type                = "OVERLAY"
   prefix              = "169.254.200.0/30"
@@ -8,4 +9,15 @@ resource "alkira_ip_reservation" "test" {
   scale_group_id      = "99a6f3db-02d5-4189-8b0a-352eaeda2e10"
   segment_id          = alkira_segment.test.id
   cxp                 = "US-WEST"
+}
+
+# Without a prefix — the backend assigns it based on prefix_type and prefix_len
+resource "alkira_ip_reservation" "backend_assigned_prefix" {
+  name           = "test-backend"
+  type           = "OVERLAY"
+  prefix_type    = "APIPA"
+  prefix_len     = 30
+  scale_group_id = "99a6f3db-02d5-4189-8b0a-352eaeda2e10"
+  segment_id     = alkira_segment.test.id
+  cxp            = "US-WEST"
 }
