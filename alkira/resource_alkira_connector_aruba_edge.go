7b584770db53745622c49ec95dc730dca56d0a18
shrey-pasari <168513003+shrey-pasari@users.noreply.github.com>
2026-03-10 12:12:43 +0530

    [AK-65414]: Add scalegroup support to Aruba Edge connector (#547)

--- alkira/resource_alkira_connector_aruba_edge.go
+++ alkira/resource_alkira_connector_aruba_edge.go
@@ -63,11 +63,6 @@ func resourceAlkiraConnectorArubaEdge() *schema.Resource {
 							Type:     schema.TypeString,
 							Required: true,
 						},
-						"scale_group_id": {
-							Description: "The ID of the scale group associated with the connector.",
-							Type:        schema.TypeString,
-							Optional:    true,
-						},
 						"aruba_edge_connect_segment": {
 							Description: "The segment of the Aruba Edge connector.",
 							Type:        schema.TypeString,
@@ -206,10 +201,10 @@ func resourceAlkiraConnectorArubaEdge() *schema.Resource {
 				Optional: true,
 				Default:  true,
 			},
-			"scale_group_id": {
-				Description: "The ID of the scale group associated with the connector.",
-				Type:        schema.TypeString,
-				Optional:    true,
+            "scale_group_id": {
+                Description: "The ID of the scale group associated with the connector.",
+             	Type:        schema.TypeString,
+             	Optional:    true,
 			},
 		},
 	}
