157a68dd6b2fb6f97f9ad9ad231cd456937d5e86
Asim Ali Khan <146973802+asim-ak@users.noreply.github.com>
2026-04-13 17:19:39 +0530

    [CLONE-64] AK-66765: Fix AWS VPC connector drift from backend-defaulted routing fields (#606)

--- alkira/resource_alkira_connector_aws_vpc_helper.go
+++ alkira/resource_alkira_connector_aws_vpc_helper.go
@@ -39,7 +39,10 @@ func setAwsVpcRoutingOptions(connector *alkira.ConnectorAwsVpc, d *schema.Resour
 // setAwsVpcExportPrefixes sets export-related fields from ExportOptions
 func setAwsVpcExportPrefixes(exportOptions interface{}, d *schema.ResourceData) {
 	if exportOptions == nil {
-		log.Printf("[DEBUG] Export options is nil, skipping export prefixes")
+		log.Printf("[DEBUG] Export options is nil, clearing export prefixes")
+		d.Set("vpc_cidr", []interface{}{})
+		d.Set("vpc_subnet", []interface{}{})
+		d.Set("overlay_subnets", []interface{}{})
 		return
 	}
 
@@ -77,16 +80,11 @@ func setAwsVpcExportPrefixes(exportOptions interface{}, d *schema.ResourceData) 
 		}
 	}
 
-	// Only set non-empty fields to preserve config when field is not in config
-	if len(cidrList) > 0 {
-		d.Set("vpc_cidr", cidrList)
-	}
-	if len(subnetList) > 0 {
-		d.Set("vpc_subnet", subnetList)
-	}
-	if len(overlaySubnets) > 0 {
-		d.Set("overlay_subnets", overlaySubnets)
-	}
+	// Always set all export fields because they are Computed
+	// Backend returns userInputPrefixes with type entries; empty set if none specified
+	d.Set("vpc_cidr", cidrList)
+	d.Set("vpc_subnet", subnetList)
+	d.Set("overlay_subnets", overlaySubnets)
 }
 
 // setAwsVpcImportRouteTables sets vpc_route_table from ImportOptions
@@ -106,10 +104,6 @@ func setAwsVpcImportRouteTables(importOptions interface{}, d *schema.ResourceDat
 	var importOpts alkira.ImportOptions
 	if err := json.Unmarshal(importJSON, &importOpts); err != nil {
 		log.Printf("[ERROR] Failed to unmarshal ImportOptions: %v", err)
-		return
-	}
-
-	if len(importOpts.RouteTables) == 0 {
 		return
 	}
 
